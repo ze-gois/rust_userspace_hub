583e2e2f4dd0301b2ab61802b433878d807298ed
José Gois <ze.gois.00@gmail.com>
2025-09-06 20:41:09 -0300

    initing main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
-use userspace;
-use userspace::info;
-use userspace::target;
+#![allow(incomplete_features)]
+#![allow(unused_assignments)]
+#![feature(generic_const_exprs)]
+#![feature(generic_const_items)]
+
+// use userspace;
+// use userspace::info;
+// use userspace::target;
+pub struct Origin;
+
+ample::trait_implement_primitives!();
+
+// use ample::traits::Bytes;
+
+// ample::trait_implement_primitive_bytes!(u32, u8);
 
 fn main() {
-    info!("do");
-    println!("doto")
+    // info!("do");
+    ample::macros::r#struct!(pub Estrutura {
+        a : u32,
+        b : bool,
+        c : Option<u32>
+    });
+
+    let estrutura = Estrutura {
+        a: 42,
+        b: true,
+        c: Some(23),
+    };
+
+    let bytes = <Estrutura as ample::traits::Bytes<crate::Origin, crate::Origin>>::to_bytes(
+        &estrutura, true,
+    );
+
+    let estrutura2 =
+        <Estrutura as ample::traits::Bytes<crate::Origin, crate::Origin>>::from_bytes(bytes, true);
+
+    println!("doto {:?}", estrutura2);
 }
