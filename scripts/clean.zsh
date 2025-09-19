1e35d2c83fcf7dc066f7d89761fde7614c9686fa
José Gois <ze.gois.00@gmail.com>
2025-09-19 01:42:05 -0300

    initing main

---
+++ scripts/clean.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/zsh
+DIR=$1
+if [[ -z "$DIR" ]]; then
+    DIR=$(pwd)
+fi
+for toml in $(find "$DIR" -type f -name "Cargo.toml"); do
+    echo cargo clean --manifest-path=$toml
+    cargo clean --manifest-path=$toml
+done
